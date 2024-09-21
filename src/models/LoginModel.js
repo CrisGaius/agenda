58e31a9cc3345137e85496a1beb5fe0178ea5916
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-09-21 14:58:37 -0300

    fazendo o hash da senha utilizando o pacote bcrypt.js

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -18,11 +19,19 @@ class Login {
   async register() {
     this.valida()
     if (this.errors.length > 0) return
-    try {
-      this.user = await LoginModel.create(this.body)
-    } catch (e) {
-      console.error(e)
-    }
+
+    await this.usuarioJaExiste()
+
+    if (this.errors.length > 0) return
+
+    const salt = bcryptjs.genSaltSync()
+    this.body.senha = bcryptjs.hashSync(this.body.senha, salt)
+    this.user = await LoginModel.create(this.body)
+  }
+
+  async usuarioJaExiste() {
+    const user = await LoginModel.findOne({ email: this.body.email })
+    if (user) this.errors.push('Já existe alguém utilizando este email.')
   }
 
   valida() {
